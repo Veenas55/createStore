d566c72b8818e522876ab3ef158be50d53028031
veena <veena55.samudrala@gmail.com>
2026-09-04 11:13:28 +0530

    Initial commit

---
+++ src/main/java/com/example/createstore/CreateStoreApplication.java
@@ -0,0 +1,18 @@
+package com.example.createstore;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateStoreApplication {
+
+    public static void main(String[] args) {
+        //configuration of dotenv-java
+        Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+        dotenv.entries().forEach((entry -> System.setProperty( entry.getKey(), entry.getValue()) ));
+
+        SpringApplication.run(CreateStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/createstore/CreateStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.createstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
